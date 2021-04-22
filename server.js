a8b99cd33f8235ca2897f77e28417ef70dee5d23
AKSHAY KUMAR VERMA <akshaykumarnitj@gmail.com>
2021-04-22 21:16:27 +0530

    port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const app=express()
-const PORT=process.env.PORT|4444
+
+const PORT=process.env.PORT||4444;
+
 app.use('/',express.static(__dirname+'/public'))
 
 ans="";
